8384c91ecb38ff9348f53e24318c3c9846f00edb
hadja41 <hadhawabah@gmail.com>
2024-07-26 10:35:05 +0200

    Ajout del'exercice javafx css

--- src/main/java/fr/afpa/democss/App.java
+++ src/main/java/fr/afpa/democss/App.java
@@ -8,15 +8,19 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+  
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
